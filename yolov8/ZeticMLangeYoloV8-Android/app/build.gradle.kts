a684625c33376b757cfa0359de35e6aff5042bbb
doyeop-zeticai <doyeop@zetic.ai>
2024-11-21 20:26:59 +0900

    update demo code for optimized yolo

--- yolov8/ZeticMLangeYoloV8-Android/app/build.gradle.kts
+++ yolov8/ZeticMLangeYoloV8-Android/app/build.gradle.kts
@@ -4,28 +4,25 @@ plugins {
 }
 
 android {
-    namespace = "com.example.zeticmlangeyolov8android"
+    namespace = "com.zeticai.zeticmlangeyolov8androidjava"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.zeticmlangeyolov8android"
+        applicationId = "com.zeticai.zeticmlangeyolov8androidjava"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
             )
         }
     }
@@ -36,34 +33,22 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
     packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        jniLibs {
+            useLegacyPackaging = true
         }
     }
 }
 
 dependencies {
+    implementation(files("libs/zeticlibs/zeticMLange.aar"))
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
