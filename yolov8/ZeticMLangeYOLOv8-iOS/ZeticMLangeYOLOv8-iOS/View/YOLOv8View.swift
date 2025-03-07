49fe49d7629e47f704e89cee23dc5e1b7bcfdca9
changgeunJi <changgeun@zetic.ai>
2025-03-07 13:15:12 +0900

    fix yolo box in iOS

--- yolov8/ZeticMLangeYOLOv8-iOS/ZeticMLangeYOLOv8-iOS/View/YOLOv8View.swift
+++ yolov8/ZeticMLangeYOLOv8-iOS/ZeticMLangeYOLOv8-iOS/View/YOLOv8View.swift
@@ -9,12 +9,23 @@ struct YOLOv8View: View {
     @StateObject private var cameraModel: CameraModel = CameraModel(.back, .frame)
     private let classes = CocoConfig.readClasses()
     
+    @State private var aspectRatio: CGFloat = 16.0 / 9.0 // Changed when resolution set
+    
     var body: some View {
         let cameraResolution = cameraModel.resolution
         GeometryReader { geometry in
             ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
+                
+                let width = geometry.size.width
+                let height = geometry.size.height
+                let targetWidth = width
+                let targetHeight = targetWidth * aspectRatio
+                
                 CameraPreview(session: cameraModel.session)
-                    .ignoresSafeArea()
+                    .customFrame(targetWidth: targetWidth, targetHeight: targetHeight,
+                                 geometryWidth: width, geometryHeight: height)
+                
             }
             ZStack {
                 if cameraModel.frame != nil && cameraModel.timestamp != nil {
@@ -24,14 +35,24 @@ struct YOLOv8View: View {
                     let color = getClassColor(classId: result.classId)
                     let confidence = String(format: "%.2f", result.confidence)
                     let label = "\(classes[Int(result.classId)]) \(confidence)"
+                    
+                    let width = geometry.size.width
+                    let height = geometry.size.height
+                    let targetWidth = width
+                    let targetHeight = targetWidth * aspectRatio
+                    
                     let box = calculateBox(for: result, in: geometry, cameraResolution)
+                    
+                    
+                    let box = calculateBox(for: result, in: CGSize(width: targetWidth, height: targetHeight), cameraResolution)
                     
                     DetectionBoxView(
                         result: result,
                         color: color,
                         label: label,
-                        box: box
-                    )
+                        box: box)
+                            .customFrame(targetWidth: targetWidth, targetHeight: targetHeight,
+                                         geometryWidth: width, geometryHeight: height)
                 }
                 
             }
@@ -42,6 +63,8 @@ struct YOLOv8View: View {
         }.onDisappear {
             yolov8.close()
             cameraModel.close()
+        }.onChange(of: cameraModel.resolution) { newResolution in
+            self.aspectRatio = newResolution.width / newResolution.height
         }
     }
     
@@ -52,13 +75,15 @@ struct YOLOv8View: View {
         return Color(red: r, green: g, blue: b)
     }
     
-    private func calculateBox(for result: YOLOv8Result, in geometry: GeometryProxy, _ cameraResolution: CGSize) -> [CGFloat] {
-        [
-            CGFloat(result.box[0]) * (geometry.size.width / cameraResolution.height),
-            CGFloat(result.box[1]) * (geometry.size.height / cameraResolution.width),
-            CGFloat(result.box[2]) * (geometry.size.width / cameraResolution.height),
-            CGFloat(result.box[3]) * (geometry.size.height / cameraResolution.width)
+    private func calculateBox(for result: YOLOv8Result, in targetSize: CGSize, _ cameraResolution: CGSize) -> [CGFloat] {
+        print(targetSize)
+        let ret = [
+            CGFloat(result.box[0]) * (targetSize.width / cameraResolution.height),
+            CGFloat(result.box[1]) * (targetSize.height / cameraResolution.width),
+            CGFloat(result.box[2]) * (targetSize.width / cameraResolution.height),
+            CGFloat(result.box[3]) * (targetSize.height / cameraResolution.width)
         ]
+        return ret
     }
     
 }
