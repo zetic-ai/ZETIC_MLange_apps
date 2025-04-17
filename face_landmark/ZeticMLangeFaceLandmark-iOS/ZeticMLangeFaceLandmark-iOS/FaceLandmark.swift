839a2a7b2cc5bb181b07f16c5239ca90124ee216
doyeop-zeticai <doyeop@zetic.ai>
2025-04-17 10:40:51 +0900

    update face landmark ios

--- face_landmark/ZeticMLangeFaceLandmark-iOS/ZeticMLangeFaceLandmark-iOS/FaceLandmark.swift
+++ face_landmark/ZeticMLangeFaceLandmark-iOS/ZeticMLangeFaceLandmark-iOS/FaceLandmark.swift
@@ -2,15 +2,15 @@ import Foundation
 import UIKit
 import ZeticMLange
 
-class FaceLandmark {
-    private let model = ZeticMLangeModel("face_landmark")!
+class FaceLandmark: ObservableObject {
+    private let model = (try? ZeticMLangeModel("debug_cb6cb12939644316888f333523e42622", "c6a4ff77eee74c42bfc04e5afcbd712a"))!
     private let wrapper = FaceLandmarkWrapper()
     
     func process(input: FaceLandmarkInput) -> FaceLandmarkOutput {
         do {
             let preprocess = wrapper.preprocess(input.image, input.roi)
             if (preprocess.isEmpty) {
-                throw ZeticMLangeError("preprocess failed")
+                throw ZeticMLangeError("FaceLandmark", "preprocess failed")
             }
             try model.run([preprocess])
             var modelOutput = model.getOutputDataArray()
