815aa3647bfaaffb95f87a792e2b5354b483691f
yeonseok-zeticai <yeonseok@zeticai.com>
2026-01-23 12:01:39 +0900

    feat: update TextAnonymizer to use adapt_mlange_key.sh script
    - Replace hardcoded keys with YOUR_PERSONAL_ACCESS_TOKEN placeholder
    - Update adapt_mlange_key.sh to support:
      - iOS Swift: tokenKey, personalKey, privateTokenKey patterns
      - iOS xcscheme: ZETIC_ACCESS_TOKEN environment variable
      - Android Kotlin: MLANGE_PERSONAL_ACCESS_TOKEN constant
      - Android Kotlin: tokenKey variable pattern
      - Android: ZeticMLangeModel constructor pattern

--- apps/TextAnonymizer/Android/app/src/main/java/com/zeticai/textanonymizer/Constants.kt
+++ apps/TextAnonymizer/Android/app/src/main/java/com/zeticai/textanonymizer/Constants.kt
@@ -1,7 +1,7 @@
 package com.zeticai.textanonymizer
 
 object Constants {
-    const val MLANGE_PERSONAL_ACCESS_TOKEN = "ztp_374183520dc34acea84fe112ea577350"
+    const val MLANGE_PERSONAL_ACCESS_TOKEN = "YOUR_PERSONAL_ACCESS_TOKEN"
 
     // Model name: jathin-zetic/tanaos-text-anonymizer
     const val MODEL_NAME = "jathin-zetic/tanaos-text-anonymizer"
